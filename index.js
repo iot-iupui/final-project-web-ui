0121e4375711f52973dd6c28bf4d026c867ccf9e
nrhubbar <nrhubbar@iupui.edu>
2017-12-03 03:17:03 -0800

    Got it to work properly

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let sendForm = () => {
     const options = {
         method: 'POST',
-        body: {
-            'phonenumber': document.getElementById("phonenumber").value,
-            'laundryIp': document.getElementById("ipAddress").value,
-            'name': document.getElementById("name").value
-        },
+        body: JSON.stringify({
+            phonenumber: document.getElementById("phonenumber").value,
+            laundryIp: document.getElementById("ipAddress").value,
+            name: document.getElementById("name").value
+        }),
         headers: new Headers({
-            'Content-Type': "application/json"
+            'Content-Type': "application/json; charset=utf-8",
+            'Accept': 'application/json'
         }),
         mode: 'cors'
     };
@@ -15,6 +16,14 @@ let sendForm = () => {
     //Send Form
     fetch("http://159.203.108.254:3000/laundry/new", options).then((res) => {
         console.dir(res);
+        document.getElementById("container").innerHTML = `
+        <div class="card text-center">
+        <div class="card-body">
+            <h4 class="card-title">Registered!</h4>
+            <p class="card-text">Your Dryer has been registered</p>
+        </div>
+    </div>
+        `
     }).catch((err) => {
         console.log(err);
     });
